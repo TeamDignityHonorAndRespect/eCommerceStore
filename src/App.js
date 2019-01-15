e5862d89229e97379902b316ccf3cdb43761dae7
logansimonsen <logansimonsen@hotmail.com>
2019-01-15 11:32:59 -0700

    added example menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Menu from './components/Menu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Menu />
         <header className="App-header">
           <p>hi</p>
         </header>
