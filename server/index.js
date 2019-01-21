17de4f78c92c95571458edc22be65c71d79e869d
logansimonsen <logansimonsen@hotmail.com>
2019-01-21 10:17:50 -0700

    successfully connected to Heroku/postgres database and tested /api/users endpoint which returns all users from the users endpoint

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 var _ = require('underscore');
-// const massive = require("massive");
+const massive = require("massive");
 
 const array = ['item1', 'item2', 'item3', 'item4']
 
@@ -13,13 +13,22 @@ const app = express();
 
 // app.use(express.static(`${__dirname}/../build`));  // for production use
 
-//Connect to database
-// massive(CONNECTION_STRING)
-//   .then(db => {
-//     // console.log(db);
-//     app.set("db", db);
-//   })
-//   .catch(console.log)
+// Connect to database
+massive({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    database: process.env.DATABASE,
+    user: process.env.USERID,
+    password: process.env.PASSWORD,
+    ssl: process.env.SSL,
+    port: process.env.PORT
+})
+    .then(db => {
+        // console.log(db);
+        app.set("db", db);
+    })
+    .catch(console.log)
+
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -30,6 +39,14 @@ app.get('/random', (req, res) => res.send(_.sample(array)))
 //return all items
 app.get('/all', (req, res) => res.send(array))
 
+app.get("/api/users", (req, res) => {
+    app
+        .get("db")
+        .getAllUsers()
+        .then(response => {
+            res.status(200).json(response);
+        });
+});
 // const path = require("path");  //for production use
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../build/index.html"));
