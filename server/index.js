10ed6c6d08b3e72304f279e497ce59dd6a8f0987
Preston Bayle <prestonbayle@gmail.com>
2019-01-25 16:05:24 -0700

    added some stuff for auth0

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
 const express = require("express");
+const session = require('express-session');
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const _ = require('underscore');
 const massive = require("massive");
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+
+//middleware
+const isAuthenticated = require('./middleware/isAuthenticated')
 
 const array = ['item1', 'item2', 'item3', 'item4']
 
@@ -32,6 +38,90 @@ massive({
 
 app.use(bodyParser.json());
 app.use(cors());
+
+
+// COMMENTED OUT CODE IS FOR AUTH0 SET UP
+
+// app.use(session({
+//     secret: process.env.SESSION_SECRET,
+//     saveUninitialized: true,
+//     resave: false
+//     })
+//   );
+  
+//   app.use(passport.initialize());
+//   app.use(passport.session());
+  
+//   passport.use(
+//     new Auth0Strategy(
+//       {
+//         domain: process.env.AUTH_DOMAIN,
+//         clientID: process.env.AUTH_CLIENT_ID,
+//         clientSecret: process.env.AUTH_CLIENT_SECRET,
+//         callbackURL: process.env.AUTH_CALLBACK,
+//         scope: "openid profile"
+//       },
+//       (accessToken, refreshToken, extraParams, profile, done) => {
+  
+//         const db = app.get('db');
+//         db.get_user_by_auth_id({ auth_id: profile.id }).then(results => {
+//           let user = results[0];
+  
+//           if (user) {
+//             return done(null, user)
+//           } else {
+//             let userObj = {
+//               username: profile.displayName,
+//               auth_id: profile.id
+//             }
+            
+//             db.create_user(userObj).then(results => {
+//               let user = results[0];
+//               return done(null, user)
+//             })
+//           }
+//         });
+//       }
+//     )
+//   );
+  
+//   passport.serializeUser((user, done) => {
+//     return done(null, user.id);
+//   });
+  
+//   passport.deserializeUser((id, done) => {
+//     const db = app.get('db');
+  
+//     db.get_user_by_id({ id }).then(results => {
+//       let user = results[0];
+//       return done(null, user);
+//     });
+//   });
+  
+//   app.get("/auth", passport.authenticate("auth0"));
+//   app.get(
+//     "/auth/callback",
+//     passport.authenticate("auth0", {
+//       successRedirect: "/#/resorts",
+//       failureRedirect: "/#/"
+//     })
+//   );
+  
+//   app.get('/auth/logout', (req, res) => {
+//     req.logout()
+//     res.redirect('/#/')
+//   });
+  
+//   app.get("/auth/me", (req, res) => {
+//     if (req.isAuthenticated()) {
+//       return res.send(req.user);
+//     } else {
+//       return res.status(404).send("user not authenticated");
+//     }
+//   });
+  
+
+
 
 //get random item
 app.get('/random', (req, res) => res.send(_.sample(array)))
